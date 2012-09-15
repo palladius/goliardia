480399f93c740ea5c45aa4e5da75e58e8a07bf00
Riccardo GCarlesso <riccardo.carlesso@gmail.com>
2012-09-15 13:22:26 +0100

    init is more Ubuntu-like and less Riccardo-like :)

--- bin/init.rb
+++ bin/init.rb
@@ -7,16 +7,16 @@ def init_goliardia(dir)
 	# cose cambiabili da palo:
 	%w[ citta ordini persone ].each { |d| 
 			puts "+ Rendo cambiabile da Palo la directory img-#{d}..."
-			`chown riccardo.aiutanti ./immagini/#{d}`
+			`chown www-data.www-data ./immagini/#{d}`
 			`chmod 775 ./immagini/#{d}` 
 			puts '... e il suo contenuto (muto):'
-			`chown riccardo.aiutanti ./immagini/#{d}/* 2>/dev/null`
+			`chown www-data.www-data ./immagini/#{d}/* 2>/dev/null`
 			`chmod 664 ./immagini/#{d}/* 2>/dev/null `
 			
 			puts "Var scrivibili da apache (per darla via devi essere ROOT, mica come le donne):"
 			%w[ var/log var/state var/prova uploads/thumb ].each{ |dtutti|
 				`mkdir -p #{dtutti}`
-				`chown riccardo.www-data #{dtutti}`  # devi essere root
+				`chown www-data.www-data #{dtutti}`  # devi essere root
 				`chmod 775 #{dtutti}`
 			}
 	}
