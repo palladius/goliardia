b53c654ec0d9039280e54b1ee242f74590ef394c
Palladius BonTon <palladiusbonton@gmail.com>
2019-05-05 14:32:50 +0000

    added crontab dump and better logging

--- bin/gsutil-rsync-logs.sh
+++ bin/gsutil-rsync-logs.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-gsutil -m rsync -dr /var/www/www.goliardia.it/var/log/ gs://www.goliardia.it/backups/logs/`hostname --fqdn`/
+gsutil -m rsync -dr /var/www/www.goliardia.it/var/log/ gs://www.goliardia.it/backups/www.goliardia.it/logs/`hostname --fqdn`/app/
+gsutil -m rsync -dr /var/log/apache2/ gs://www.goliardia.it/backups/www.goliardia.it/logs/`hostname --fqdn`/apache/
