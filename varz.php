a11ecc5f3364b768afaefdd3067147bc293c9f11
Riccardo Carlesso <palladiusbonton+goliardiaprod@gmail.com>
2020-09-19 20:14:24 +0000

    working first varz

--- varz.php
+++ varz.php
@@ -1,3 +1,13 @@
-#TODO(RICC): for pull of LIVE resources from instrumentation.
-utentiAttivi: 42
-sobenme: 42
+<?php
+header("Content-Type: text/plain");
+
+include "conf/setup.php";
+include "funzioni.php";
+
+?>
+ClientIp: <? echo $_SERVER['REMOTE_ADDR']; ?> 
+UtentiAttivi: <? echo count(explode('$', getApplication("UTENTI_ORA"))) ?>
+
+UtentiVerbose: <? echo getApplication("UTENTI_ORA") ?>
+
+FakeLatency: 42
