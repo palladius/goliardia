40f9bcd65c767a166b86e14c5d8b4a1483b46a54
Riccardo Carlesso <ricc+derek.zrh@google.com>
2020-09-20 09:40:08 +0200

    nuovoMessaggio

--- constants.php
+++ constants.php
@@ -4,8 +4,8 @@
 	//$webmaster_email = 'palladius@goliardia.org';
 	//$webmaster_email = 'palladiusbonton+goliardiapuntoit@gmail.com';
 	$webmaster_email = 'goliardiapuntoit+iscrizione@gmail.com';
-	$WEBMASTERMAIL = "palladius.bon.ton+goliardia@gmail.com"; 
-	$MESSAGGINO_HEADER = "Scusate, addi' 20150202 il sito e' stato raso al suolo e l'ho dovuto migrare.. ora funge. Per problemi, per favore filate un baco <a href='https://distillery.fogbugz.com/default.asp?pg=pgPublicEdit'>qui</a>. Tuttora c'e' un baco sugli apostrofi. Se avete problemi a inserire/modificare dati per favore provate a non usare il single quote (') ma magari qualunque altro carattere (grazie Criceto). Anzi, potete usare il quote basta RADDOPPIARLO ('') e ne avrete uno solo. Grazie per la pazienza..." ;
+	$WEBMASTERMAIL = "riccardo.carl.esso+goliardia@gmail.com"; 
+	$MESSAGGINO_HEADER = "Scusate, addi' 2020-09-15 il Load Balancer si e' invornito e ho dovuto migrare l'intero sito.. ora funge. Grazie a Manolus e NPP per l'aiuto. Per problemi, per favore filate un baco <a href='https://distillery.fogbugz.com/default.asp?pg=pgPublicEdit'>qui</a>. Tuttora c'e' un baco sugli apostrofi. Se avete problemi a inserire/modificare dati per favore provate a non usare il single quote (') ma magari qualunque altro carattere (grazie Criceto). Anzi, potete usare il quote basta RADDOPPIARLO ('') e ne avrete uno solo. Grazie per la pazienza... -- IronPal" ;
 
 
 ?>
