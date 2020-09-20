02e741d0aa96d4ce2f6d2dcc1825bea8990f68b1
Riccardo Carlesso <ricc+derek.zrh@google.com>
2020-09-20 09:36:59 +0200

    hostname

--- varz.php
+++ varz.php
@@ -5,10 +5,11 @@ include "conf/setup.php";
 include "funzioni.php";
 
 ?>
-ClientIp: <? echo $_SERVER['REMOTE_ADDR']; ?> 
+ClientIp: <? echo $_SERVER['REMOTE_ADDR']; ?>
 UtentiAttivi: <? echo count(explode('$', getApplication("UTENTI_ORA"))) , "\n" ?>
 UtentiVerbose: <? echo getApplication("UTENTI_ORA") , "\n"?>
+hostname: <? echo gethostname() , "\n" ?>
+VERSION: <? echo file_get_contents("VERSION"); ?>
+# TODO
 FakeLatency: 42
 TimestampLatestMessage: TODO(Ricc)
-VERSION: <? echo file_get_contents("VERSION"); ?>
-
