efa7618c9a571e724bf43bbd4de831f48c2f8508
Riccardo Carlesso <ricc+derek.zrh@google.com>
2020-09-20 09:34:10 +0200

    version fie works

--- varz.php
+++ varz.php
@@ -7,8 +7,8 @@ include "funzioni.php";
 ?>
 ClientIp: <? echo $_SERVER['REMOTE_ADDR']; ?> 
 UtentiAttivi: <? echo count(explode('$', getApplication("UTENTI_ORA"))) , "\n" ?>
-UtentiVerbose: <? echo getApplication("UTENTI_ORA") ?>
-
+UtentiVerbose: <? echo getApplication("UTENTI_ORA") , "\n"?>
 FakeLatency: 42
 TimestampLatestMessage: TODO(Ricc)
-VERSION: TODO read file
+VERSION: <? echo file_get_contents("VERSION"); ?>
+
